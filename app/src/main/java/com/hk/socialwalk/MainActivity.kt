a224787892d0744f56e0efffb9e9b64ece53e8bc
princegupta25 <princesmart.gupta@gmail.com>
2022-06-08 23:19:20 +0530

    bottomNav

--- app/src/main/java/com/hk/socialwalk/MainActivity.kt
+++ app/src/main/java/com/hk/socialwalk/MainActivity.kt
@@ -2,10 +2,51 @@ package com.hk.socialwalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.Fragments.CreateFragment
+import com.Fragments.FeedFragment
+import com.Fragments.ProfileFragment
+import com.hk.socialwalk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val createFragment = CreateFragment()
+    private val feedFragment = FeedFragment()
+    private val profileFragemnt = ProfileFragment()
+
+    private lateinit var binding: ActivityMainBinding
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        replaceFragment(feedFragment)
+
+        binding.bottomNav.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.feed-> replaceFragment(feedFragment)
+                R.id.create-> replaceFragment(createFragment)
+                R.id.profile-> replaceFragment(profileFragemnt)
+            }
+            true
+        }
+
+
     }
+
+
+    fun replaceFragment(fragment:Fragment){
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container,fragment)
+            transaction.commit()
+        }
+    }
+
+
 }
